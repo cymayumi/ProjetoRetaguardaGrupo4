7aff55cc607c27bd2e88d69e1a641a049ddef615
Carol <carolbenetao@gmail.com>
2021-01-27 22:02:22 -0300

    Servlet Exame e page forme-exame.jsp

---
+++ WebContent/pages/form-exame.jsp
@@ -0,0 +1,36 @@
+
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Alterar Exame</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+	<h1>Atualizar Exame</h1>
+	<form action="/RetaguardaGrupo4/ExameServlet" method="post">
+	
+	<div class="form-group">
+			<label for="id-exame">Tipo de Exame</label>
+			<input type="text" name="nomeExame" id="id-exame" class="form-control" value="${exame.dsTipoExame}">
+	</div>
+
+
+		<input type="hidden" name="id" value="${exame.idTipoExame}">
+		<input type="hidden" name="acao" value="editar">
+		
+		<div class="text-center mt-4">
+			<a class="btn btn-danger" href="/RetaguardaGrupo4/ExameServlet">CANCELAR</a>
+			<input type="submit" value="SALVAR ALTERAÇÕES" class="btn btn-primary">
+		</div>
+	</form>
+</div>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ src/com/rd/quartaturma/grupo4/servlet/ExameServlet.java
@@ -0,0 +1,165 @@
+package com.rd.quartaturma.grupo4.servlet;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.rd.quartaturma.grupo4.entity.ExameEntity;
+import com.rd.quartaturma.grupo4.vo.Exame;
+
+@WebServlet("/ExameServlet")
+public class ExameServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public ExameServlet() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String acao = request.getParameter("acao");
+
+		if (acao == null) {
+			this.listarExames(request, response);
+		} else if (acao.equals("editar")) {
+			this.editarExame(request, response);
+		} else if (acao.equals("remover")) {
+			this.excluirExame(request, response);
+		} else if (acao.equals("novo")) {
+			RequestDispatcher rd = request.getRequestDispatcher("/pages/novo-exame.jsp");
+			rd.forward(request, response);
+		}
+	}
+
+	private void listarExames(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		RequestDispatcher rd = null;
+
+		try {
+			EntityManager em = CrudEntityManager.getEntityManager();
+			Query query = em.createNamedQuery("Exame.findAll", ExameEntity.class);
+
+			List<ExameEntity> exameEntity = query.getResultList();
+
+			List<Exame> exames = new ArrayList<>();
+
+			for (ExameEntity entity : exameEntity) {
+				Exame exame = new Exame();
+				
+				exame.setIdTipoExame(BigInteger.valueOf(entity.getIdTipoExame().intValue()));
+				exame.setDsTipoExame(entity.getDsTipoExame());
+
+				exames.add(exame);
+			}
+
+			request.setAttribute("exames", exames);
+
+			rd = request.getRequestDispatcher("/pages/lista-exames.jsp");
+			rd.forward(request, response);
+
+		} catch (Exception e) {
+			request.setAttribute("erro", "Erro ao consultar lista de exames!");
+			rd = request.getRequestDispatcher("/pages/erro-validacao.jsp");
+			e.printStackTrace();
+		}
+	}
+
+	private void editarExame(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		RequestDispatcher rd = null;
+
+		try {
+			String id = request.getParameter("id");
+			EntityManager em = CrudEntityManager.getEntityManager();
+			ExameEntity exameEntity = em.find(ExameEntity.class, new BigInteger(id));
+
+			Exame exame = new Exame();
+
+			exame.setIdTipoExame(exameEntity.getIdTipoExame());
+			exame.setDsTipoExame(exameEntity.getDsTipoExame());
+
+			request.setAttribute("exame", exame);
+
+			rd = request.getRequestDispatcher("/pages/form-exame.jsp");
+			rd.forward(request, response);
+
+		} catch (Exception e) {
+			request.setAttribute("erro", "Erro ao editar exames!");
+			rd = request.getRequestDispatcher("/pages/erro-validacao.jsp");
+
+			rd.forward(request, response);
+			e.printStackTrace();
+			e.printStackTrace();
+		}
+
+	}
+
+	private void excluirExame(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String id = request.getParameter("id");
+
+		EntityManager em = CrudEntityManager.getEntityManager();
+		ExameEntity exameEntity = em.find(ExameEntity.class, new BigInteger(id));
+		em.getTransaction().begin();
+		em.remove(exameEntity);
+		em.getTransaction().commit();
+
+		listarExames(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String acao = request.getParameter("acao");
+
+		if (acao.equals("editar"))
+			this.atualizarExame(request, response);
+		else if (acao.equals("novo"))
+			this.inserirExame(request, response);
+
+		listarExames(request, response);
+	}
+
+	private void atualizarExame(HttpServletRequest request, HttpServletResponse response) {
+		
+		BigInteger id = BigInteger.valueOf(Integer.valueOf(request.getParameter("id")).intValue());
+		String exameNome = request.getParameter("nomeExame");
+
+
+		EntityManager em = CrudEntityManager.getEntityManager();
+		ExameEntity exameEntity = em.find(ExameEntity.class, id);
+		
+		exameEntity.setDsTipoExame(exameNome);
+
+		em.getTransaction().begin();
+		em.merge(exameEntity);
+		em.getTransaction().commit();
+	}
+
+	private void inserirExame(HttpServletRequest request, HttpServletResponse response) {
+
+		String exameNome = request.getParameter("nomeExame");
+
+		ExameEntity exameEntity = new ExameEntity();
+		exameEntity.setDsTipoExame(exameNome);
+		
+		EntityManager em = CrudEntityManager.getEntityManager();
+		
+		em.getTransaction().begin();
+		em.persist(exameEntity);
+		em.getTransaction().commit();
+	}
+
+}
